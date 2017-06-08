4f205e402c34fd0e2acad2f91a8a8647255334e9
Lauro de Lacerda <laurodelacerda@gmail.com>
2017-06-08 13:33:08 -0300

    Add changes on PSEmergency Application.

--- apps/PSEmergency.cc
+++ apps/PSEmergency.cc
@@ -59,6 +59,21 @@ void PSEmergency::onWSA(WaveServiceAdvertisment* wsa) {
     //Your application has received a service advertisement from another car or RSU
     //code for handling the message goes here, see TraciDemo11p.cc for examples
 
+    /* @lauro
+     *  Trocas de canal ocorrem a partir do recebimento de WSAs
+     *  TODO Aqui verificar o PSID da msg recebida para seleção de canal
+     *  Em seu exemplo, o Veins usa o psid 42 (não alocado)
+     *  TODO EmergencyWarning usa o PSID decimal 12
+     * */
+
+    if (currentSubscribedServiceId == -1) {
+        mac->changeServiceChannel(wsa->getTargetChannel());
+        currentSubscribedServiceId = wsa->getPsid();
+        if  (currentOfferedServiceId != wsa->getPsid()) {
+            stopService();
+            startService((Channels::ChannelNumber) wsa->getTargetChannel(), wsa->getPsid(), "Emergency Warning");
+        }
+    }
 }
 
 void PSEmergency::handleSelfMsg(cMessage* msg) {
@@ -66,11 +81,51 @@ void PSEmergency::handleSelfMsg(cMessage* msg) {
     //this method is for self messages (mostly timers)
     //it is important to call the BaseWaveApplLayer function for BSM and WSM transmission
 
+    /* @lauro
+     * O Veins trabalha o channel switching especialmente nesta função
+     * Para a aplicação de emergência talvez fosse necessário ter troca de canal
+     *
+     * Configurações no omnetpp.ini seriam:
+     * *.**.nic.mac1609_4.useServiceChannel = true
+     * *.node[*].appl.dataOnSch = true
+     * *.rsu[*].appl.dataOnSch = true
+    **/
+
+    if (WaveShortMessage* wsm = dynamic_cast<WaveShortMessage*>(msg)) {
+        //send this message on the service channel until the counter is 3 or higher.
+        //this code only runs when channel switching is enabled
+        sendDown(wsm->dup());
+        wsm->setSerial(wsm->getSerial() +1);
+        if (wsm->getSerial() >= 3) {
+            //stop service advertisements
+            stopService();
+            delete(wsm);
+        }
+        else {
+            scheduleAt(simTime()+1, wsm);
+        }
+    }
+    else {
+        BaseWaveApplLayer::handleSelfMsg(msg);
+    }
 }
 
 void PSEmergency::handlePositionUpdate(cObject* obj) {
     BaseWaveApplLayer::handlePositionUpdate(obj);
     //the vehicle has moved. Code that reacts to new positions goes here.
     //member variables such as currentPosition and currentSpeed are updated in the parent class
+
+    if (onDuty){
+    // @lauro: Neste caso, apenas a ambulância ficará vermelha.
+        findHost()->getDisplayString().updateWith("r=16,red");
+        WaveShortMessage* wsm = new WaveShortMessage();
+        populateWSM(wsm);
+        wsm->setWsmData(gps->getCourse(numberOfRoads).c_str());
+        //wsm->setSecurityType(); TODO Implementar segurança
+        wsm->setPsid(12);
+        wsm->setPsc("Give way! Public Safety vehicle is on Duty!");
+        wsm->setChannelNumber(Channels::CRIT_SOL);
+    }
+
 
 }
